09dd530521c615c79641233c8d86a57d342fd901
Layne Johnson <layne@findum.com>
2021-07-26 12:19:12 -0400

    Configure networking

--- Sun Shower/Models/WeatherManager.swift
+++ Sun Shower/Models/WeatherManager.swift
@@ -13,6 +13,28 @@ struct WeatherManager {
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
-        print(urlString)
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        // Create a URL with URL initializer
+        if let url = URL(string: urlString) {
+            
+            // Create a URLSession (object that performs networking)
+            let session = URLSession(configuration: .default)
+            
+            // Give session a task
+            // .dataTask returns a URLSessionDataTask
+            // Completion handler takes a function as a value; Data?, URLResponse, and Error? are function parameters
+            let task = session.dataTask(with: url, completionHandler: <#T##(Data?, URLResponse?, Error?) -> Void#>)
+            
+            // Start task
+            task.resume()
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) -> Void {
+        
     }
 }
